eb69ac0b3a4ca26bd2bde27fc30c6241319e9a7d
juanchuletas <gamj@xanum.uam.mx>
2020-02-04 07:37:16 -0600

    Alg

--- Cpp_code/ConvexHull/hull_main.cpp
+++ Cpp_code/ConvexHull/hull_main.cpp
@@ -2,7 +2,62 @@
 #include <iomanip>
 #include "../Points/Point2D.hpp"
 #include <ctime>
+#include <vector>
 
+
+
+void createConvexHull(Point2D *points,int N)
+{
+	double target = points[0].x;
+	int leftmost;
+	std::vector<Point2D> hull;
+	for(int i=1; i<N; i++)
+	{
+		if(target>points[i].x)
+		{
+			target = points[i].x;
+			leftmost = i;
+		}
+
+	}
+	std::cout<<"The left most point is "<<target<<"\n";
+	int vertex = leftmost;
+	int guess_point;
+	do
+	{
+		hull.push_back(points[vertex]);
+
+		guess_point = (vertex + 1 )%N;
+		for(int i = 0; i<N; i++)
+		{
+			if(points[i].getOrientation(points[vertex],points[guess_point])==1)
+			{
+				guess_point = i;
+			}
+		}
+		vertex = guess_point;
+
+	}while(vertex!=leftmost);
+
+	std::cout<<"The convex hull is: \n";
+	for(int i=0; i<hull.size(); i++)
+	{
+		std::cout<<hull[i].x<<", "<<hull[i].y<<"\n";
+		
+	}
+
+	
+}
+void createVTK(Point2D *points, int N)
+{
+	std::cout<<"DATASET POYDATA\n";
+	std::cout<<"POINTS "<<N<<" float\n";
+
+	for(int i=0; i<N; i++)
+	{
+		std::cout<<points[i].x<<" "<<points[i].y<<"\n";
+	}
+}
 
 int main()
 {
@@ -12,17 +67,22 @@ int main()
 
 
 	std::cout<<"The random number is: "<<secretNum<<"\n";
+	int N = 20;
+	Point2D points[N];
 
-	Point2D points[3];
-
-	for(int i=0; i<3; i++)
+	for(int i=0; i<N; i++)
 	{
 		double x = ((double)rand()/(double)RAND_MAX)*10.0;
 		double y = ((double)rand()/(double)RAND_MAX)*10.0;
 		points[i].setValues(x,y);
 		points[i].print(cout);
 	}
-	std::cout<<points[1].x<<"\n";
+	createConvexHull(points,N);
+	createVTK(points,N);
+
+
+
+	
 
 
 
